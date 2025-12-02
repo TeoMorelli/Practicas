2b351bf02316af8567bb6e3669c00d226a40bfed
Teo Morelli <teomorellii@gmail.com>
2025-12-01 23:33:24 -0300

    cambio2

--- practica/BST.go
+++ practica/BST.go
@@ -166,3 +166,36 @@ func tieneUnHijo(n *Nodo) bool {
 }
 
 /*7️⃣ Verificar si el árbol es completo*/
+
+func (bst *BST) estaCompleto(n *Nodo) bool {
+	if bst.Root == nil {
+		return false
+	}
+
+	cola := []*Nodo{bst.Root}
+	hijoVacio := false
+
+	for len(cola) > 0 {
+		actual := cola[0]
+		cola = cola[1:]
+
+		if actual.left != nil {
+			if hijoVacio {
+				return false
+			}
+			cola = append(cola, actual.left)
+		} else {
+			hijoVacio = true
+		}
+
+		if actual.right != nil {
+			if hijoVacio {
+				return false
+			}
+			cola = append(cola, actual.right)
+		} else {
+			hijoVacio = true
+		}
+	}
+	return true
+}
