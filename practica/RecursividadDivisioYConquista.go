7a89761fa4514f127a59f35c701833746b51e34c
TeoMorelli <155658191+TeoMorelli@users.noreply.github.com>
2025-12-09 18:00:01 -0300

    Update RecursividadDivisioYConquista.go

--- practica/RecursividadDivisioYConquista.go
+++ practica/RecursividadDivisioYConquista.go
@@ -1,1 +1,28 @@
 package practica
+
+/*Contar dígitos de un número
+
+Implementá una función recursiva que reciba un número entero positivo y devuelva cuántos dígitos tiene.
+*/
+
+func digitos( num int ) int {
+  if num < 10 {
+    return 1
+  }
+  return 1 + digitos(num / 10)
+}
+
+/*
+Verificar si un arreglo es palíndromo
+
+Dado un arreglo de enteros, escribir una función recursiva que determine si es palíndromo 
+(se lee igual de izquierda a derecha que de derecha a izquierda).
+*/
+
+func esPalindromo( arr[] int ) bool {
+  
+}
+
+
+
+
