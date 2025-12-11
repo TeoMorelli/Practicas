67047a76af5deef0b1569135e6a7d50327edfa9b
Teo Morelli <teomorellii@gmail.com>
2025-12-10 22:44:37 -0300

    cambio8

--- practica/patronesDeDiseño.go
+++ practica/patronesDeDiseño.go
@@ -1,1 +1,14 @@
 package practica
+
+/*
+Los patrones de diseño son basicamente formas de reutilizacion de codigo, estos ofrecen una respuesta pra problema recurrentes
+*/
+
+/*Patron adapter, se utiliza para reutilizar codigo de una interfaz que no coincida con lo que espera el sistema*/
+
+/*
+Convertir un enchufe europeo a enchufe americano.
+
+Tenés una interfaz que representa un EnchufeAmericano.
+Tenés una clase existente EnchufeEuropeo, incompatible.
+*/
